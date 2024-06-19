a5dd02cd6efc4ea4ce31c9eeb43217835f5ab2dd
kiseokkm <311kskim@gmail.com>
2024-06-19 20:03:06 +0900

    Feat:init

---
+++ src/main/java/com/sparta/greeypeople/GreeyPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.greeypeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeyPeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreeyPeopleApplication.class, args);
+    }
+
+}
