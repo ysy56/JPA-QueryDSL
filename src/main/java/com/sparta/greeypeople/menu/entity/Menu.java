3af914ef7f4595a5d8837203d4fd49843fbc7ad7
yj <belle521834@gmail.com>
2024-06-20 19:38:30 +0900

    🎉 add : Menu entity, repository 추가

---
+++ src/main/java/com/sparta/greeypeople/menu/entity/Menu.java
@@ -0,0 +1,26 @@
+package com.sparta.greeypeople.menu.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "menu")
+@NoArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "menu", length = 50 , nullable = false)
+    private String menu;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "store_id", nullable = false)
+//    private Store store;
+}
