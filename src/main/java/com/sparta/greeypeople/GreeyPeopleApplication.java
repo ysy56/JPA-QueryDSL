599aba23f5bf80855a08a20f88688867adee4a30
kiseokkm <311kskim@gmail.com>
2024-06-20 10:43:50 +0900

    Feat:init

--- src/main/java/com/sparta/greeypeople/GreeyPeopleApplication.java
+++ src/main/java/com/sparta/greeypeople/GreeyPeopleApplication.java
@@ -2,8 +2,10 @@ package com.sparta.greeypeople;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GreeyPeopleApplication {
 
     public static void main(String[] args) {
