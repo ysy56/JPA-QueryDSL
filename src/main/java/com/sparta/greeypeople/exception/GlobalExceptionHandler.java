911ae5b376ce301b668698af65ea7d534fc00584
moony <78634780+ysy56@users.noreply.github.com>
2024-06-20 12:09:54 +0900

    🎉 add : GlobalExceptionHandler 추가

---
+++ src/main/java/com/sparta/greeypeople/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.sparta.greeypeople.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+}
