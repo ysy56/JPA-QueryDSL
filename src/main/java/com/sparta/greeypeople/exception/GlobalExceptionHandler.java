00868aa4d9d4707f15642173fd63d84871db431b
moony <78634780+ysy56@users.noreply.github.com>
2024-06-20 12:11:34 +0900

    🎉 add : BadRequestException 추가

--- src/main/java/com/sparta/greeypeople/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/greeypeople/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.sparta.greeypeople.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,5 +11,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
+
+    /**
+     * 클라이언트로 부터 잘못된 요청 값이 들어온 경우
+     * @param ex : BadRequestException[custom]
+     * @return : error message, HttpStatus.BAD_REQUEST => 400
+     */
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> badRequestException(BadRequestException ex) {
+        log.error("{}", ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 }
