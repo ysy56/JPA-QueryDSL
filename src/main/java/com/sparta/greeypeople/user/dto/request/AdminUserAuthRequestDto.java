b35f8cff4430476bc78756fe4f1ff8697980526f
moony <78634780+ysy56@users.noreply.github.com>
2024-06-21 00:57:29 +0900

    🩹 fix : 유효성 검사 추가

---
+++ src/main/java/com/sparta/greeypeople/user/dto/request/AdminUserAuthRequestDto.java
@@ -0,0 +1,14 @@
+package com.sparta.greeypeople.user.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminUserAuthRequestDto {
+
+    @NotBlank(message = "변경할 권한을 입력해주세요")
+    private String userAuthority;
+
+}
