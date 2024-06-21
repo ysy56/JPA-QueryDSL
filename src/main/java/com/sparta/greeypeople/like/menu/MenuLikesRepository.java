48c49f8b7b14d78e090307b1137469545fceb953
박시현 <sihyun615@naver.com>
2024-06-21 11:24:51 +0900

    🩹 fix : 메뉴 좋아요 관련 코드 수정

--- src/main/java/com/sparta/greeypeople/like/menu/MenuLikesRepository.java
+++ src/main/java/com/sparta/greeypeople/like/menu/MenuLikesRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.greeypeople.like.menu;
 
 import com.sparta.greeypeople.menu.entity.Menu;
+import com.sparta.greeypeople.user.entity.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/sparta/greeypeople/like/menu/MenuLikesService.java
+++ src/main/java/com/sparta/greeypeople/like/menu/MenuLikesService.java
@@ -1,5 +1,8 @@
 package com.sparta.greeypeople.like.menu;
 
+import com.sparta.greeypeople.menu.repository.MenuRepository;
+import com.sparta.greeypeople.user.entity.User;
+import com.sparta.greeypeople.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,14 +21,16 @@ import lombok.extern.slf4j.Slf4j;
 public class MenuLikesService {
 
 	private final MenuLikesRepository menuLikesRepository;
-	private final UserService userService;
-	private final MenuService menuService;
+	private final UserRepository userRepository;
+	private final MenuRepository menuRepository;
 
 	@Transactional
 	public void addMenuLike(Long menuId, User user) {
 
-		User foundUser = userService.findById(user.getId());
-		Menu foundMenu = menuService.findById(menuId);
+		User foundUser = userRepository.findById(user.getId()).orElseThrow(() ->
+				new DataNotFoundException("해당 사용자가 존재하지 않습니다."));
+		Menu foundMenu = menuRepository.findById(menuId).orElseThrow(() ->
+			new DataNotFoundException("해당 메뉴가 존재하지 않습니다."));
 
 		if (foundUser.getUserId().equals(foundMenu.getUser().getUserId())) {
 			throw new BadRequestException("자신이 등록한 메뉴에는 좋아요를 남길 수 없습니다.");
@@ -47,7 +52,8 @@ public class MenuLikesService {
 		MenuLikes foundlike = menuLikesRepository.findById(menuLikeId).orElseThrow(
 			() -> new DataNotFoundException("해당 좋아요가 존재하지 않습니다."));
 
-		Menu foundMenu = menuService.findById(foundlike.getMenu().getId());
+		Menu foundMenu = menuRepository.findById(foundlike.getMenu().getId()).orElseThrow(
+			() -> new DataNotFoundException("해당 메뉴가 존재하지 않습니다."));
 
 		if (!(user.getUserId().equals(foundlike.getUser().getUserId()))) {
 			throw new ForbiddenException("다른 사람의 좋아요는 삭제할 수 없습니다.");
