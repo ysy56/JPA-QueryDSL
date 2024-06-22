aef66a0b95a62149e9f5f15f86e077a383f68f10
yj <belle521834@gmail.com>
2024-06-22 23:31:41 +0900

    ✨ update   : 리뷰 예외 추가

--- src/main/java/com/sparta/greeypeople/review/dto/request/ReviewRequestDto.java
+++ src/main/java/com/sparta/greeypeople/review/dto/request/ReviewRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.greeypeople.review.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class ReviewRequestDto {
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 }
