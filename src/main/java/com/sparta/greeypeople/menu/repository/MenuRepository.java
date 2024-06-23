e270fa6057731d1d9ee2f4d4b2abb98cb66c0a9d
yj <belle521834@gmail.com>
2024-06-23 21:18:44 +0900

    🎉 add  : 사용자 가게 메뉴 조회

--- src/main/java/com/sparta/greeypeople/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/greeypeople/menu/repository/MenuRepository.java
@@ -1,7 +1,9 @@
 package com.sparta.greeypeople.menu.repository;
 
 import com.sparta.greeypeople.menu.entity.Menu;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findByStoreId(Long storeId);
 }
