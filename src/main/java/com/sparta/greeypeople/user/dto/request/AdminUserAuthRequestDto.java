5c9d6a8e9f311d36d4727607d38f7b0fb3dc7cf3
moony <78634780+ysy56@users.noreply.github.com>
2024-06-23 14:43:43 +0900

    🩹 fix : 권한 변경 요청 값 자료형 변경

--- src/main/java/com/sparta/greeypeople/user/dto/request/AdminUserAuthRequestDto.java
+++ src/main/java/com/sparta/greeypeople/user/dto/request/AdminUserAuthRequestDto.java
@@ -1,6 +1,8 @@
 package com.sparta.greeypeople.user.dto.request;
 
+import com.sparta.greeypeople.user.enumeration.UserAuth;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class AdminUserAuthRequestDto {
 
     @NotBlank(message = "변경할 권한을 입력해주세요")
-    private String userAuth;
+    @Pattern(regexp = "^(ADMIN|USER)$\n", message = "(USER, ADMIN) 대문자로만 입력해주세요.")
+    private UserAuth userAuth;
 
 }
