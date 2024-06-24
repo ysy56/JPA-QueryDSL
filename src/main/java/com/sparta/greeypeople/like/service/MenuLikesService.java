18afb1c89d1c80ef4825b21dce41c5e8c8e66b11
박시현 <sihyun615@naver.com>
2024-06-24 17:43:10 +0900

    🩹 fix : 가게 및 메뉴 검증과 중복 코드에 대한 메서드 생성

--- src/main/java/com/sparta/greeypeople/like/service/MenuLikesService.java
+++ src/main/java/com/sparta/greeypeople/like/service/MenuLikesService.java
@@ -1,21 +1,21 @@
 package com.sparta.greeypeople.like.service;
 
-import com.sparta.greeypeople.like.entity.MenuLikes;
-import com.sparta.greeypeople.like.repository.MenuLikesRepository;
-import com.sparta.greeypeople.menu.repository.MenuRepository;
-import com.sparta.greeypeople.user.entity.User;
-import com.sparta.greeypeople.user.repository.UserRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.sparta.greeypeople.exception.BadRequestException;
 import com.sparta.greeypeople.exception.DataNotFoundException;
 import com.sparta.greeypeople.exception.ForbiddenException;
 import com.sparta.greeypeople.exception.ViolatedException;
+import com.sparta.greeypeople.like.entity.MenuLikes;
+import com.sparta.greeypeople.like.repository.MenuLikesRepository;
 import com.sparta.greeypeople.menu.entity.Menu;
-
+import com.sparta.greeypeople.menu.repository.MenuRepository;
+import com.sparta.greeypeople.store.entity.Store;
+import com.sparta.greeypeople.store.repository.StoreRepository;
+import com.sparta.greeypeople.user.entity.User;
+import com.sparta.greeypeople.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -25,44 +25,56 @@ public class MenuLikesService {
 	private final MenuLikesRepository menuLikesRepository;
 	private final UserRepository userRepository;
 	private final MenuRepository menuRepository;
+	private final StoreRepository storeRepository;
 
 	@Transactional
-	public void addMenuLike(Long menuId, User user) {
-
-		User foundUser = userRepository.findById(user.getId()).orElseThrow(() ->
-				new DataNotFoundException("해당 사용자가 존재하지 않습니다."));
-		Menu foundMenu = menuRepository.findById(menuId).orElseThrow(() ->
-			new DataNotFoundException("해당 메뉴가 존재하지 않습니다."));
+	public void addMenuLike(Long storeId, Long menuId, User user) {
+		User foundUser = validateUser(user);
+		Menu foundMenu = validateStoreAndMenu(storeId, menuId);
 
 		if (foundUser.getUserId().equals(foundMenu.getUser().getUserId())) {
 			throw new BadRequestException("자신이 등록한 메뉴에는 좋아요를 남길 수 없습니다.");
 		}
 
-		MenuLikes menuLikes = new MenuLikes(foundUser, foundMenu);
-
 		if (menuLikesRepository.findByUserAndMenu(foundUser, foundMenu).isPresent()) {
 			throw new ViolatedException("이미 좋아요를 누른 메뉴입니다.");
 		}
 
+		MenuLikes menuLikes = new MenuLikes(foundUser, foundMenu);
 		menuLikesRepository.save(menuLikes);
 		foundMenu.addLike();
 	}
 
 	@Transactional
-	public void removeMenuLike(Long menuLikeId, User user) {
-
-		MenuLikes foundlike = menuLikesRepository.findById(menuLikeId).orElseThrow(
+	public void removeMenuLike(Long storeId, Long menuLikeId, User user) {
+		User foundUser = validateUser(user);
+		MenuLikes foundLike = menuLikesRepository.findById(menuLikeId).orElseThrow(
 			() -> new DataNotFoundException("해당 좋아요가 존재하지 않습니다."));
+		Menu foundMenu = validateStoreAndMenu(storeId, foundLike.getMenu().getId());
 
-		Menu foundMenu = menuRepository.findById(foundlike.getMenu().getId()).orElseThrow(
-			() -> new DataNotFoundException("해당 메뉴가 존재하지 않습니다."));
-
-		if (!(user.getUserId().equals(foundlike.getUser().getUserId()))) {
+		if (!foundUser.getUserId().equals(foundLike.getUser().getUserId())) {
 			throw new ForbiddenException("다른 사람의 좋아요는 삭제할 수 없습니다.");
 		}
 
-		menuLikesRepository.delete(foundlike);
+		menuLikesRepository.delete(foundLike);
 		foundMenu.minusLike();
 	}
 
+	private User validateUser(User user) {
+		return userRepository.findById(user.getId()).orElseThrow(() ->
+			new DataNotFoundException("해당 사용자가 존재하지 않습니다."));
+	}
+
+	private Menu validateStoreAndMenu(Long storeId, Long menuId) {
+		Store store = storeRepository.findById(storeId).orElseThrow(() ->
+			new DataNotFoundException("해당 가게가 존재하지 않습니다."));
+		Menu foundMenu = menuRepository.findById(menuId).orElseThrow(() ->
+			new DataNotFoundException("해당 메뉴가 존재하지 않습니다."));
+
+		if (!foundMenu.getStore().getId().equals(store.getId())) {
+			throw new BadRequestException("메뉴가 해당 가게에 존재하지 않습니다.");
+		}
+
+		return foundMenu;
+	}
 }
