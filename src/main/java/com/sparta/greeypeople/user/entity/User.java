1e918f11cfdfd84a877a42e3be21d010d274563f
kiseokkm <311kskim@gmail.com>
2024-06-24 12:09:44 +0900

    feat: User Entity 통합

--- src/main/java/com/sparta/greeypeople/user/entity/User.java
+++ src/main/java/com/sparta/greeypeople/user/entity/User.java
@@ -26,7 +26,7 @@ public class User extends TimeStamp {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 50)
     private String userId;
 
     @Column(nullable = false)
@@ -58,6 +58,13 @@ public class User extends TimeStamp {
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<Review> comments = new ArrayList<>();
 
+    private Long kakaoId;
+
+    @ElementCollection(fetch = FetchType.EAGER) // FetchType 설정 추가
+    @CollectionTable(name = "past_passwords", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "password")
+    private List<String> pastPasswords = new ArrayList<>();
+
     public User(SignupRequestDto requestDto, UserStatus userStatus, UserAuth userAuth) {
         this.userId = requestDto.getUserId();
         this.password = requestDto.getPassword();
@@ -66,6 +73,18 @@ public class User extends TimeStamp {
         this.intro = requestDto.getIntro();
         this.userStatus = userStatus;
         this.userAuth = userAuth;
+        this.pastPasswords.add(this.password);
+    }
+
+    public User(String userId, String password, String userName, String email, UserStatus userStatus, UserAuth userAuth, Long kakaoId) {
+        this.userId = userId;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
+        this.userStatus = userStatus;
+        this.userAuth = userAuth;
+        this.kakaoId = kakaoId;
+        this.pastPasswords.add(this.password);
     }
 
     public void updateUserStatus(UserStatus userStatus) {
@@ -77,6 +96,10 @@ public class User extends TimeStamp {
     }
 
     public void updatePassword(String newPassword) {
+        if (this.pastPasswords.size() >= 3) {
+            this.pastPasswords.remove(0);
+        }
+        this.pastPasswords.add(newPassword);
         this.password = newPassword;
     }
 
@@ -99,5 +122,10 @@ public class User extends TimeStamp {
 
     public void updateAuth(UserAuth userAuth) {
         this.userAuth = userAuth;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
     }
 }
